d7c8dbe716c001cc279940c2bb032b5b403765fd
Lam Nguyen <lamcnguyen89@gmail.com>
2025-10-26 22:59:04 -0400

    Replaced deprecated functions for assignment 04 with modern functions

--- 01_Assignments/Assignment_02/Homework 2_Materials/TransformImage.m
+++ 01_Assignments/Assignment_02/Homework 2_Materials/TransformImage.m
@@ -3,15 +3,18 @@ function outim = TransformImage(im, H)
 % the transformed image outim
 %
 
-% Matlab function imtransform assumes transpose of H as input
-tform = maketform('projective',H');
-% Next line returns the x and y coordinates of the bounding box of the 
-% transformed image through H
-[boxx, boxy]=tformfwd(tform, [1 1 size(im,2) size(im,2)], [1 size(im,1) 1 size(im,1)]);
+% Modern MATLAB uses projective2d instead of maketform
+tform = projective2d(H);
+% Get the output limits of the transformed image
+[xlim, ylim] = outputLimits(tform, [1 size(im,2)], [1 size(im,1)]);
 % Find the minimum and maximum x and y coordinates of the bounding box
-minx=min(boxx); maxx=max(boxx);
-miny=min(boxy); maxy=max(boxy);
-% Transform the input image
-outim =imtransform(im,tform,'XData',[minx maxx],'YData',[miny maxy],'Size',[size(im,1),round(size(im,1)*(maxx-minx)/(maxy-miny))]);
+minx = xlim(1); maxx = xlim(2);
+miny = ylim(1); maxy = ylim(2);
+% Create output reference object
+width = round(size(im,1)*(maxx-minx)/(maxy-miny));
+height = size(im,1);
+Rout = imref2d([height width], [minx maxx], [miny maxy]);
+% Transform the input image using imwarp
+outim = imwarp(im, tform, 'OutputView', Rout);
 
 end
--- 01_Assignments/Assignment_04/stitch2images.m
+++ 01_Assignments/Assignment_04/stitch2images.m
@@ -5,16 +5,18 @@ function outim = stitch2images(im1, im2, H)
 %
 %
 
-T = maketform('projective',double(H'));
-[~, xdata, ydata] = imtransform(im1,T);
+T = projective2d(double(H));
+[xlim, ylim] = outputLimits(T, [1 size(im1,2)], [1 size(im1,1)]);
+xdata = [xlim(1) xlim(2)];
+ydata = [ylim(1) ylim(2)];
 
 Outxdata=[min(1,xdata(1)) max(size(im2,2), xdata(2))];
 outydata=[min(1,ydata(1)) max(size(im2,1), ydata(2))];
 
-warpedim1 = imtransform(im1, maketform('projective',double(H')),...
-    'XData',Outxdata,'YData',outydata);
-warpedim2 = imtransform(im2, maketform('affine',eye(3)),...
-    'XData',Outxdata,'YData',outydata);
+Rout = imref2d([diff(outydata) diff(Outxdata)], Outxdata, outydata);
+warpedim1 = imwarp(im1, T, 'OutputView', Rout);
+T2 = affine2d(eye(3));
+warpedim2 = imwarp(im2, T2, 'OutputView', Rout);
 outim = warpedim1 + warpedim2;
 overlap = (warpedim1 > 0.0) & (warpedim2 > 0.0);
 
